dfe8558e27b6cbcb44d31979f0bf2682dd149020
empereurfranck95 <empereurfranck95@gmail.com>
2021-10-27 02:51:37 +0200

    test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,9 @@ import { RevendeursComponent } from './revendeurs/revendeurs.component';
 
 const routes: Routes = [
  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+ { path: 'clients', component: DashboardComponent, canActivate: [AuthguardGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthguardGuard] },
   { path: 'notfound', component: NotfoundComponent, canActivate: [AuthguardGuard] },
-  { path: 'clients', component: DashboardComponent, canActivate: [AuthguardGuard] },
   { path: 'clients/:id', component: ClientdetailsComponent, canActivate: [AuthguardGuard] },
   { path: 'revendeurs', component: RevendeursComponent, canActivate: [AuthguardGuard] },
   { path: 'commerciaux', component: CommerciauxComponent, canActivate: [AuthguardGuard] },
